d62c6bf5210c405000c2bbb9d1a425d217aeb4c3
anican <anirudhc@cs.washington.edu>
2020-07-27 13:27:00 -0700

    adding updated models code, original was a bit too hacky this one is easier to scale and run multiple experiments for

--- models/vgg.py
+++ models/vgg.py
@@ -20,31 +20,43 @@ class VGG(Model):
         VGG._make_fc(self.model, args)
 
     def call(self, x):
-        print(x.shape)
+        # print(x.shape)
         x = self.model(x)
-        print(x.shape)
+        # print(x.shape)
         return x
 
     @staticmethod
     def _make_fc(classifier, args):
-        # TODO: currently only supports block size of 2
         conv_width: int = args.conv_width
+        num_block: int = args.num_block
         num_dense: int = args.num_dense
         weight_decay: float = args.weight_decay
-        if conv_width == 64:
-            if num_dense == 1:
-                classifier.add(layers.Dense(10))
-            else:
-                classifier.add(layers.Dense(4096, kernel_regularizer=l2(weight_decay)))
-                classifier.add(layers.Activation('relu'))
-                classifier.add(layers.Dense(10))
+        if num_dense == 1:
+            classifier.add(layers.Dense(10))
+        elif conv_width == 64:
+            num_flat_features = 4096
+            if num_block == 4:
+                num_flat_features = 256
+            classifier.add(layers.Dense(num_flat_features, kernel_regularizer=l2(weight_decay)))
+        elif conv_width == 128:
+            num_flat_features = 8192
+            if num_block == 4:
+                num_flat_features = 512
+            classifier.add(layers.Dense(num_flat_features, kernel_regularizer=l2(weight_decay)))
         elif conv_width == 256:
-            if num_dense == 1:
-                classifier.add(layers.Dense(10))
-            else:
-                classifier.add(layers.Dense(16384, kernel_regularizer=l2(weight_decay)))
-                classifier.add(layers.Activation('relu'))
-                classifier.add(layers.Dense(10))
+            num_flat_features = 16384
+            if num_block == 4:
+                num_flat_features = 1024
+            classifier.add(layers.Dense(num_flat_features, kernel_regularizer=l2(weight_decay)))
+        elif conv_width == 512:
+            num_flat_features = 32768
+            if num_block == 4:
+                num_flat_features = 2048
+            classifier.add(layers.Dense(num_flat_features, kernel_regularizer=l2(weight_decay)))
+        # Final ReLU + classification head
+        # Final ReLU + classification head
+        classifier.add(layers.Activation('relu'))
+        classifier.add(layers.Dense(10))
 
     @staticmethod
     def _make_layers(features, args):
@@ -77,9 +89,9 @@ class VGG(Model):
 
 def _test():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--conv_width', type=int, default=64)
+    parser.add_argument('--conv_width', type=int, default=128)
     parser.add_argument('--dropout', type=float, default=0.0)
-    parser.add_argument('--num_block', type=int, default=2)
+    parser.add_argument('--num_block', type=int, default=4)
     parser.add_argument('--num_dense', type=int, default=2)
     parser.add_argument('--weight_decay', type=float, default=0.0)
     hparams = parser.parse_args()
@@ -89,10 +101,11 @@ def _test():
     x = np.random.randn(500, 32, 32, 3)
     out = net(x)
 
-    print(net.summary())
-    h5_path = os.path.join(os.getcwd(), 'weights.h5')
-    net.save_weights(h5_path)
+    # print(net.summary())
+    # h5_path = os.path.join(os.getcwd(), 'weights.h5')
+    # net.save_weights(h5_path)
     print('ready...')
+
 
 if __name__ == '__main__':
     _test()
